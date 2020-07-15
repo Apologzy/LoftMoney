967587d8f5ebec3e9606b244c800df58419f19f3
Apologzy <shapor.k.l@mail.ru>
2020-07-15 21:02:08 +1000

    Create AddItemActivity

---
+++ app/src/main/java/com/shapor/loftmoney/AddItemActivity.java
@@ -0,0 +1,26 @@
+package com.shapor.loftmoney;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        try {
+            Intent intent = new Intent(AddItemActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } catch(Exception e) {
+            //default
+        }
+    }
+}
