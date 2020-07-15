673f52e388856f3a45bec20a8389235bcf6dfb34
Apologzy <shapor.k.l@mail.ru>
2020-07-15 15:44:08 +1000

    Create HelloWorld

--- app/src/main/java/com/shapor/loftmoney/MainActivity.java
+++ app/src/main/java/com/shapor/loftmoney/MainActivity.java
@@ -3,12 +3,17 @@ package com.shapor.loftmoney;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView helloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helloWorld = (TextView) findViewById(R.id.tv_hello_world);
+        helloWorld.setText("Мы победим:" + "\n" + "Титры");
     }
 }
